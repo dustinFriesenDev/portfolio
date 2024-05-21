f0aef95a77a05788836c9b73ec07bfd000302e21
Dustin Dev <94726570+dustinFriesenDev@users.noreply.github.com>
2024-05-21 09:31:02 -0400

    set up gh-pages with Router.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
+import { Routes, Route, } from "react-router-dom";
 import Nav from "./components/navbar/Nav";
 import Home from "./components/home/Home";
 import Disney from "./components/disney-princesses/Disney";
@@ -9,14 +9,12 @@ import './App.css';
 function App() {
   return (
     <main>
-    <Router>
       <Routes>
         <Route exact path="/" element={<Nav />} />
         <Route index element={<Home />} />
         <Route path="/disney_princess_quiz" element={<Disney />} />
         <Route path="/superheroes_quiz" element={<Superheroes />} />
       </Routes>
-    </Router>
     </main>
   );
 }
